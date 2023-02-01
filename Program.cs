0550fefc9eff9a32427e224584042c43a3781209
Tikhon <loki_deleoni@live.com>
2023-02-02 00:56:36 +0300

    add zadacha 10 & 13

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите число 2 ");
+string strNumber2 = Console.ReadLine();
+
+int numA = Convert.ToInt32(strNumber1);
+int numB = Convert.ToInt32(strNumber2);
