a6253daa2229188f6aa3a22ca3bddb40972d0e92
Tikhon <loki_deleoni@live.com>
2023-02-02 00:59:13 +0300

    add update file to last commit

--- Program.cs
+++ Program.cs
@@ -5,9 +5,80 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.WriteLine("Задача 10");
+Console.WriteLine("********");
+Console.WriteLine("Enter 3-digital number  ");
+string? strNumber = Console.ReadLine();
+int number = int.Parse(strNumber);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+if((number>100) && (number<999)) 
+{
+    number = number / 10;
+    number = number % 10;
+    Console.Write ("Second ditigital in entered number is ");
+    Console.WriteLine (number);
+}
+else
+{
+Console.WriteLine("Input incorrect number, plese try again.");
+}
+Console.WriteLine("********");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
 
-Console.Write("Введите число 2 ");
-string strNumber2 = Console.ReadLine();
 
-int numA = Convert.ToInt32(strNumber1);
-int numB = Convert.ToInt32(strNumber2);
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Задача 13");
+Console.WriteLine("********");
+
+Console.WriteLine("Let's play the game...");
+Console.WriteLine("Enter number and I show you 3th digital of it");
+Console.Write("Enter number ");
+
+string? strNumber2 = Console.ReadLine();
+int number2 = int.Parse(strNumber2);
+int i = 1;
+if(number2 < 0) 
+{
+    number2 = number2 * (-1); 
+    
+}
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+if (number2 > 100)
+    {
+    while(number2/i > 999)
+        {
+            i = i*10;
+        }
+    number2 = number2 / i;
+    number2 = number2 % 10;
+    Console.WriteLine ($"The 3th digital in entered number is {number2}");
+    }
+
+else 
+    {
+    Console.WriteLine ($"input number have less than three digital, plese try again");
+    }
+
+Console.WriteLine("********");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
